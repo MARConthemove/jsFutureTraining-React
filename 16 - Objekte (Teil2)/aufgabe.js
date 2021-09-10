0c180c422ff9c519640197340d5033bd75d8dce4
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-09-10 09:49:41 +0200

    Creating new student function with .this

--- 16 - Objekte (Teil2)/aufgabe.js
+++ 16 - Objekte (Teil2)/aufgabe.js
@@ -4,30 +4,30 @@
 //
 // Für eine Sprachschule sollen wir eine Teilnehmerverwaltung als ein
 // Modul entwickeln.
-// 
+//
 // 1a) Ergänze die Funktion "addStudent", sodass ein neuer Teilnehmer
 //     hinzugefügt wird. Wichtig: Achte darauf, dass die "getName"-
 //     Eigenschaft korrekt gesetzt ist!
-// 
+//
 // 1b) Ergänze die Funktion printStudents(). Für jeden Student
-//     soll die jeweilige "getName"-Funktion aufgerufen werden, 
-//     und die Namen sollen dann möglichst anschaulich (z.B. 
+//     soll die jeweilige "getName"-Funktion aufgerufen werden,
+//     und die Namen sollen dann möglichst anschaulich (z.B.
 //     untereinander) ausgegeben werden.
 
-const getStudentName = function() {
-  return this.lastname + ", " + this.firstname
+const getStudentName = function () {
+    return this.lastname + ", " + this.firstname
 }
 
 const studentModule = {
-  students: [
-    {firstname: "Max", lastname: "Mustermann", getName: getStudentName}
-  ],
-  addStudent(firstname, lastname) {
-    // Schreibe deinen Code hier!
-  },
-  printStudents() {
-    // Schreibe deinen Code hier!
-  },
+    students: [
+        { firstname: "Max", lastname: "Mustermann", getName: getStudentName },
+    ],
+    addStudent(firstname, lastname) {
+
+    },
+    printStudents() {
+        // Schreibe deinen Code hier!
+    },
 }
 
 studentModule.addStudent("Erika", "Mustermann")
@@ -42,31 +42,32 @@ studentModule.printStudents()
 // Tipp: Es sind 2 Probleme, die teilweise aufeinander aufbauen.
 //
 // Wie kannst du die Probleme lösen? Kannst du auch komplett ohne
-// Fat-Arrow-Funktionen auskommen? Wie kann .bind() dir dabei 
+// Fat-Arrow-Funktionen auskommen? Wie kann .bind() dir dabei
 // helfen?
 
 const fs = require("fs")
 
 const rocketModule = {
-  launches: [],
-  loadLaunches: () => {
-    if (this.launches.length === 0) {
-      console.log("Lese Raketenstarts ein...")
+    launches: [],
+    loadLaunches: () => {
+        if (this.launches.length === 0) {
+            console.log("Lese Raketenstarts ein...")
 
-      fs.readFile(
-        __dirname + "/spacex/launches.json", 
-        {encoding: "utf-8"}, 
-        function(err, data) {
-          this.launches = JSON.parse(data)
-        })
-    }
-  }
+            fs.readFile(
+                __dirname + "/spacex/launches.json",
+                { encoding: "utf-8" },
+                function (err, data) {
+                    this.launches = JSON.parse(data)
+                }
+            )
+        }
+    },
 }
 
 // Kommentiere folgenden Code ein!
 // rocketModule.loadLaunches()
 
-// Gebe alle Startvorgänge aus... 
+// Gebe alle Startvorgänge aus...
 setTimeout(() => {
-  console.log(rocketModule.launches)
+    console.log(rocketModule.launches)
 }, 1000)
