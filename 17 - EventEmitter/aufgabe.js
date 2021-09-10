0702f91f23fc2528a9503f33fc01537cc6109951
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-09-10 15:36:56 +0200

    created evenemitter functions with emit and on

--- 17 - EventEmitter/aufgabe.js
+++ 17 - EventEmitter/aufgabe.js
@@ -7,99 +7,122 @@
 //
 // Wir sollen eine Todo-Verwaltung entwickeln, mit der ausstehende Aufgaben für
 // ein Team von Programmierern verwaltet werden können.
-// 
+//
 // Hierbei sind folgende Methoden zu implementieren:
-// 
+//
 // 1a) Implementiere die Funktion "addTodo". Hier soll ein neues Todo
-//     hinzugefügt werden - der Wert "done" von diesem Todo soll 
+//     hinzugefügt werden - der Wert "done" von diesem Todo soll
 //     standardmäßig auf false gesetzt sein!
 //
-//     Wichtig: Es soll nur dann ein Todo hinzugefügt werden, wenn 
+//     Wichtig: Es soll nur dann ein Todo hinzugefügt werden, wenn
 //     noch kein Todo mit dem gleichen titel existiert.
-// 
-//     Wenn ein Todo hinzugefügt wird, soll das Event "add" ausgelöst 
+//
+//     Wenn ein Todo hinzugefügt wird, soll das Event "add" ausgelöst
 //     werden, als Parameter soll dann neu angelegte Todo übergeben werden.
 //
 // 1b) Implementiere die Funktion "completeTodo". Hier soll der Titel
 //     eines Todos übergeben werden.
-// 
+//
 //     Wird ein Todo dieses Titels gefunden, soll die Eigenschaft "done"
 //     dieses Todos auf den Wert true gesetzt werden.
-// 
+//
 //     Wenn ein Todo abgeschlossen wurde, soll das Event "complete"
 //     ausgelöst werden - als Parameter soll auch wieder das Todo
 //     übergeben werden.
 //
 // 1c) Ergänze die Funktion getTodoCount(). Diese Funktion soll
-//     die Anzahl der Todos zurückgeben, bei denen "done" auf false steht - 
+//     die Anzahl der Todos zurückgeben, bei denen "done" auf false steht -
 //     also die Anzahl der Todos, die noch ausstehend sind.
-// 
+//
 //     Während dieser Berechnung muss kein Event gefeuert werden.
 
-
 const todoModule = {
-  todos: [
-    {title: "HTML lernen", done: true},
-    {title: "JavaScript lernen", done: false},
-    {title: "Node.JS lernen", done: false}
-  ],
+    todos: [
+        { title: "HTML lernen", done: true },
+        { title: "JavaScript lernen", done: false },
+        { title: "Node.JS lernen", done: false },
+    ],
 
-  /**
-   * @param {string} title 
-   */
-  addTodo(title) {
-    // Ergänze deinen Code hier
-  },
+    /**
+     * @param {string} title
+     */
+    addTodo(title) {
+        for (const todo of this.todos) {
+            if (todo.title === title) {
+                return
+            }
+        }
+        const newTodo = {
+            title: title,
+            done: false
+        }
 
- /**
-  * @param {string} title 
-  */
-  completeTodo(title) {
-    // Ergänze deinen Code hier
-  },
+        this.todos.push(newTodo)
+        this.emit("add", newTodo)
+    },
 
-  /**
-   * @returns {number}
-   */
-  getTodoCount() {
-    // Ergänze deinen Code hier
-  },
+    /**
+     * @param {string} title
+     */
+    completeTodo(title) {
+        for (const todo of this.todos) {
+            if (todo.title === title && todo.done === false) {
+                todo.done = true
+                this.emit("completed", todo)
+            }
+        }
+    },
 
+    /**
+     * @returns {number}
+     */
+    getTodoCount() {
+        let todoCount = 0
+        for (const todo of this.todos) {
+            if (!todo.done) {
+                todoCount++
+            }
+        }
+        return todoCount
+    },
 
-  events: {},
-  /**
-   * Löst ein Ereignis aus. Dieser Funktion dürfen beliebig viele
-   * params übergeben werden, diese werden 1:1 an die Event-Listener
-   * durchgereicht. 
-   * 
-   * @param {string} eventName 
-   * @param {*=} params 
-   */
-  emit(eventName, param) {
-    if (eventName in this.events) {
-      for(const f of this.events[eventName]) {
-        f(param)
-      }
-    }
-  },
+    events: {},
+    /**
+     * Löst ein Ereignis aus. Dieser Funktion dürfen beliebig viele
+     * params übergeben werden, diese werden 1:1 an die Event-Listener
+     * durchgereicht.
+     *
+     * @param {string} eventName
+     * @param {*=} params
+     */
+    emit(eventName, param) {
+        if (eventName in this.events) {
+            for (const f of this.events[eventName]) {
+                f(param)
+            }
+        }
+    },
 
-  /**
-   * Registriert einen Event-Listener für das Event eventName.
-   * 
-   * @param {string} eventName 
-   * @param {Function} cb 
-   */
-  on(eventName, cb) {
-    // Hier ist die Klammersetzung ganz wichtig!
-    if (!(eventName in this.events)) {
-      this.events[eventName] = []
-    }
-    this.events[eventName].push(cb)
-  }
+    /**
+     * Registriert einen Event-Listener für das Event eventName.
+     *
+     * @param {string} eventName
+     * @param {Function} cb
+     */
+    on(eventName, cb) {
+        // Hier ist die Klammersetzung ganz wichtig!
+        if (!(eventName in this.events)) {
+            this.events[eventName] = []
+        }
+        this.events[eventName].push(cb)
+    },
 }
+// todoModule.addTodo("TEST!")
+// todoModule.completeTodo("TEST!")
+// console.log(todoModule)
 
 // Aufgabe 2
-// 
+//
 // Damit die Programmierer besser motiviert sind, soll ein Monitor
 // fortlaufend die aktuelle Anzahl der Todos anzeigen.
 //
@@ -110,12 +133,16 @@ const todoModule = {
 //     todoModule.getTodoCount() aus?
 
 
-let todoCount = todoModule.getTodoCount()
-console.log(todoCount)
+const printCurrentTodoCount = () => {
+    let todoCount = todoModule.getTodoCount()
+    console.log(todoCount)
+}
+printCurrentTodoCount()
+
+todoModule.on("add", printCurrentTodoCount)
+todoModule.on("complete", printCurrentTodoCount)
 
 // Registriere hier das Callback / den Event-Listener!
-
-
 
 todoModule.addTodo("Aufgaben abschließen")
 // Nach dem obigen Aufruf soll die aktuelle Anzahl der offenen Todos ausgegeben
