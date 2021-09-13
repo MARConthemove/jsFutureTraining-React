8367b0ef5f243ab9f85b7145921cffd0b3d8f26f
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-09-13 11:48:32 +0200

    axios init improved utility

--- 26 - API Projekt/index.js
+++ 26 - API Projekt/index.js
@@ -3,12 +3,18 @@
 // loading axios module
 const axios = require("axios")
 
-let url =
-    "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=rbPh1DHMesLmpbAVm9hdDyjJGyxfT2fRKqpqcqQX&query=Apple"
+let url = "https://api.nal.usda.gov/fdc/v1/foods/search"
 
-axios.get(url)
-    .then((response) => {
-        console.log(response)
-        for(const food of response.data.foods)
+axios
+    .get(url, {
+        params: {
+            api_key: "rbPh1DHMesLmpbAVm9hdDyjJGyxfT2fRKqpqcqQX",
+            query: "apple juice",
+        },
     })
-
+    .then((response) => {
+        // console.log(response)
+        for (const food of response.data.foods) {
+            console.log(food["fdcId"] + ": " + food["description"])
+        }
+    })
