829bb283700dda11ba4dab1c58cf7c7978a21178
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-09-13 12:14:01 +0200

    small improvements to api fetch

--- 26 - API Projekt/index.js
+++ 26 - API Projekt/index.js
@@ -5,16 +5,19 @@ const axios = require("axios")
 
 let url = "https://api.nal.usda.gov/fdc/v1/foods/search"
 
-axios
-    .get(url, {
-        params: {
-            api_key: "rbPh1DHMesLmpbAVm9hdDyjJGyxfT2fRKqpqcqQX",
-            query: "apple juice",
-        },
-    })
-    .then((response) => {
-        // console.log(response)
-        for (const food of response.data.foods) {
-            console.log(food["fdcId"] + ": " + food["description"])
-        }
-    })
+const searchFood = (term) => {
+    return axios
+        .get(url, {
+            params: {
+                api_key: "rbPh1DHMesLmpbAVm9hdDyjJGyxfT2fRKqpqcqQX",
+                query: term,
+            },
+        })
+        .then((response) => {
+            return response.data['foods']
+        })
+}
+
+searchFood("Apple, raw").then((response) => {
+    console.log(response)
+})
