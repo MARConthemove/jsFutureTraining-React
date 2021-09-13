9a178bfab879de81f30de7aac44d0999cc0f31eb
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-09-13 11:42:56 +0200

    improvement with promises to api read

--- 26 - API Projekt/index.js
+++ 26 - API Projekt/index.js
@@ -1,5 +1,14 @@
 "use strict"
 
+// loading axios module
 const axios = require("axios")
 
+let url =
+    "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=rbPh1DHMesLmpbAVm9hdDyjJGyxfT2fRKqpqcqQX&query=Apple"
+
+axios.get(url)
+    .then((response) => {
+        console.log(response)
+        for(const food of response.data.foods)
+    })
 
