62a3efe8fd1bca8ed1df53153a45e3765b0a291d
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-09-20 16:20:52 +0200

    created an event handler + listener to a react button

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -7,6 +7,10 @@ import Card from "../UI/Card"
  * @returns
  */
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("clicked!!!")
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -14,6 +18,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
