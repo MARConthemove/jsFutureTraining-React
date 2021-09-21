5c0350a696330b87aba349a8cde5d5452cab1652
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-09-21 09:12:04 +0200

    useState implemented on ExpenseItem Component

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react"
+
 import "./ExpenseItem.css"
 import ExpenseDate from "./ExpenseDate"
 import Card from "../UI/Card"
@@ -7,15 +9,23 @@ import Card from "../UI/Card"
  * @returns
  */
 const ExpenseItem = (props) => {
+  /**
+   * ReactHook -> update the title variable
+   * title = current State Value
+   * setTitle = update State Value
+   */
+  const [title, setTitle] = useState(props.title)
+
   const clickHandler = () => {
-    console.log("clicked!!!")
+    setTitle('Updated!!')
+    console.log(title)
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
