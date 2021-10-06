dfdaad7409c023dae09aeea0ea81f33824f0a6ff
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-10-06 13:07:33 +0200

    step finished

--- 14 Rest Project/src/App.js
+++ 14 Rest Project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
 import MoviesList from './components/MoviesList'
 import './App.css'
@@ -8,7 +8,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true)
     setError(null)
     try {
@@ -17,7 +17,6 @@ function App() {
         throw new Error('Something went wrong!')
       }
       const data = await response.json()
-
 
       const transformedMovies = data.results.map((movieData) => {
         return {
@@ -33,7 +32,11 @@ function App() {
       setError(error.message)
     }
     setIsLoading(false)
-  }
+  }, [])
+
+  useEffect(() => {
+    fetchMoviesHandler()
+  }, [fetchMoviesHandler])
 
   let content = <p>Found no movies.</p>
   if (movies.length > 0) {
@@ -51,9 +54,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {content}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   )
 }
