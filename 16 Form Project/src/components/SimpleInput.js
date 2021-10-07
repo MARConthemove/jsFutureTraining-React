e8d7398317c394eda806e42fa309c2c872331d69
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-10-07 11:45:16 +0200

    useState + useRef for user input changehandler

--- 16 Form Project/src/components/SimpleInput.js
+++ 16 Form Project/src/components/SimpleInput.js
@@ -1,15 +1,36 @@
+import React, { useRef, useState } from 'react'
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef()
+  const [enteredName, setEnteredName] = useState('')
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value)
+  }
+  const formSubmissionHandler = (event) => {
+    event.preventDefault()
+
+    console.log('Entered Name with useState:', enteredName)
+    const enteredValue = nameInputRef.current.value
+    console.log('Entered Name with useRef:', enteredValue)
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          ref={nameInputRef}
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler}
+        />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default SimpleInput;
+export default SimpleInput
