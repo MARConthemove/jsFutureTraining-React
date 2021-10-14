f8344967c9cdd38952f8c01052604583dd17a69d
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-10-14 11:23:49 +0200

    finished the form submission

--- 16 Form Project/src/components/BasicForm.js
+++ 16 Form Project/src/components/BasicForm.js
@@ -1,27 +1,122 @@
-const BasicForm = (props) => {
+import useBasicInput from '../hooks/use-basicInput'
 
+const BasicForm = (props) => {
+  const {
+    value: enteredFirstName,
+    isValid: enteredFirstNameIsValid,
+    hasError: firstNameInputHasError,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstNameInput,
+  } = useBasicInput((value) => value.trim() !== '')
+
+  const {
+    value: enteredLastName,
+    isValid: enteredLastNameIsValid,
+    hasError: lastNameInputHasError,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastNameInput,
+  } = useBasicInput((value) => value.trim() !== '')
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEamilInput,
+  } = useBasicInput((value) => value.includes('@'))
+
+  let formIsValid = false
+
+  if (
+    enteredFirstNameIsValid &&
+    enteredLastNameIsValid &&
+    enteredEmailIsValid
+  ) {
+    formIsValid = true
+  }
+
+  // Form submission handler:
+  const formSubmissionHandler = (event) => {
+    event.preventDefault()
+
+    if (!formIsValid) {
+      return
+    }
+
+    console.log('Form submitted!')
+    console.log('Entered FirstName: ', enteredFirstName)
+    console.log('Entered LastName: ', enteredLastName)
+    console.log('Entered Email: ', enteredEmail)
+
+    resetFirstNameInput()
+    resetLastNameInput()
+    resetEamilInput()
+  }
+
+  // CSS classes handler:
+  const firstNameInputClasses = firstNameInputHasError
+    ? 'form-control invalid'
+    : 'form-control'
+
+  const lastNameInputClasses = firstNameInputHasError
+    ? 'form-control invalid'
+    : 'form-control'
+
+  const enteredEmailClasses = firstNameInputHasError
+    ? 'form-control invalid'
+    : 'form-control'
 
   return (
-    <form>
-      <div className='control-group'>
+    <form onSubmit={formSubmissionHandler}>
+      <div className={firstNameInputClasses}>
         <div className='form-control'>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input
+            type='text'
+            id='name'
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+            value={enteredFirstName}
+          />
+          {firstNameInputHasError && (
+            <p className='error-text'>First name must not be empty</p>
+          )}
         </div>
-        <div className='form-control'>
+        <div className={lastNameInputClasses}>
           <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+          <input
+            type='text'
+            id='name'
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+            value={enteredLastName}
+          />
+          {lastNameInputHasError && (
+            <p className='error-text'>Last name must not be empty</p>
+          )}
         </div>
       </div>
-      <div className='form-control'>
+      <div className={enteredEmailClasses}>
         <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+        />
+        {emailInputHasError && (
+          <p className='error-text'>Email has to have a @ assigned</p>
+        )}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default BasicForm;
+export default BasicForm
