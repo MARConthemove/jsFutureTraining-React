498c891c64d9a665f6793f9a7431d5875b43f7a0
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-10-18 07:54:11 +0200

    new project

--- BetterFoodApp/src/components/Meals/AvailableMeals.js
+++ BetterFoodApp/src/components/Meals/AvailableMeals.js
@@ -7,17 +7,22 @@ import classes from './AvailableMeals.module.css'
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([])
   const [isLoading, setIsLoading] = useState(true)
-  const [httpError, setHttpError] = useState(null);
+  const [httpError, setHttpError] = useState()
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        'https://react-http-4be4b-default-rtdb.europe-west1.firebasedatabase.app/meals.sjon'
+        'https://react-http-4be4b-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
       )
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
 
       const responseData = await response.json()
 
       const loadedMeals = []
+
       for (const key in responseData) {
         loadedMeals.push({
           id: key,
@@ -29,13 +34,25 @@ const AvailableMeals = () => {
       setMeals(loadedMeals)
       setIsLoading(false)
     }
-    fetchMeals()
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false)
+      setHttpError(error.message)
+    })
   }, [])
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     )
   }
