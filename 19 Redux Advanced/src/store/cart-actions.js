0c3f7632c99c4fc2eff6f36759e1b8225790879e
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-11-16 10:46:47 +0100

    fetchin logic in frontend implemented

--- 19 Redux Advanced/src/store/cart-actions.js
+++ 19 Redux Advanced/src/store/cart-actions.js
@@ -1,4 +1,35 @@
+import { cartActions } from './cart-slice'
 import { uiActions } from './ui-slice'
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://react-http-4be4b-default-rtdb.europe-west1.firebasedatabase.app/cart.json'
+      )
+
+      if (!response.ok) {
+        throw new Error('Could not fetch cart data!')
+      }
+
+      const data = await response.json()
+
+      return data
+    }
+    try {
+      const cartData = await fetchData()
+      dispatch(cartActions.replaceCart(cartData))
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      )
+    }
+  }
+}
 
 export const sendCartData = (cart) => {
   return async (dispatch) => {
