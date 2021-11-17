b6e85148f1b55bd9a7a32416598431b1a248823d
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-11-17 10:47:03 +0100

    React Routing // started new project

---
+++ 20 React Routing/src/App.js
@@ -0,0 +1,21 @@
+import { Route } from 'react-router-dom'
+import Products from './components/Products'
+import Welcome from './components/Welcome'
+
+function App() {
+  return (
+    <div>
+      <Route path='/welcome'>
+        <Welcome />
+      </Route>
+      <Route path='/products'>
+        <Products />
+      </Route>
+    </div>
+  )
+}
+
+export default App
+
+// our-domain.com/ => Component A
+// our-domain.com/products => Component B
