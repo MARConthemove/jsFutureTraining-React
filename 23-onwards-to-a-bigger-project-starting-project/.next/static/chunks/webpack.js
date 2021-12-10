1f3f10538ccf12261b90ae5f06b727a3652d162b
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-12-10 12:33:18 +0100

    implemented some revalidation for data

--- 23-onwards-to-a-bigger-project-starting-project/.next/static/chunks/webpack.js
+++ 23-onwards-to-a-bigger-project-starting-project/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d8c10a95107ce874bcbf";
+/******/ 	var hotCurrentHash = "4bfa1d820ee86b3464a2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
