108954752ede62ca82a0c5840d80b07c25709c52
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-12-10 09:39:28 +0100

    created meetup List page

---
+++ 23-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -0,0 +1,25 @@
+import MeetupList from '../components/meetups/MeetupList'
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A first Meetup',
+    image: 'https://unsplash.com/photos/Nyvq2juw4_o',
+    address: 'Some address 5, 12345 Some City',
+    discription: 'This is a first meetup',
+  },
+  {
+    id: 'm2',
+    title: 'A second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    discription: 'This is a second meetup',
+  },
+]
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default HomePage
